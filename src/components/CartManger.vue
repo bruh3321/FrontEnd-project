a1440a331db60a25643a4ac99e7eab2c234c41bc
bakali1 <bakalimohammed43@gmail.com>
2025-11-17 12:46:13 +0100

    cart add

---
+++ src/components/CartManger.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+     <h2>Votre panier</h2>
+    <div class="row g-3 gap-2">
+      <CartItem v-for="p in products" :key="p.id" :product="p" class="col-6 col-md-4 col-lg-3"/>
+    <p>it's me mario</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import CartItem from './CartItem.vue';
+import { getCartProducts } from '@/services/cart';
+const products = ref([])
+
+onMounted(() => {
+  products.value = getCartProducts();
+  
+})
+</script>
